c69e092e43a805d3bf713ad3339d4a7cf0055bdc
Humdiess <hmzinchannel@gmail.com>
2024-08-16 09:56:09 +0700

    docs: buat ini menjadi dinamis

---
+++ components/navbar.php
@@ -0,0 +1,35 @@
+<nav class="navbar border d-flex justify-content-center" id="main-navbar">
+    <div class="d-flex justify-content-between align-items-center w-100 nav-main px-5">
+        <div class="nav-left">
+            <a class="navbar-brand fw-bold fs-3" href="#">Madura<span class="text-danger fw-bold fs-6">.shop</span></a>
+        </div>
+        <div class="nav-center">
+            <div class="nav-search">
+                <input type="text" placeholder="Search" class="form-control rounded-pill">
+            </div>
+        </div>
+        <div class="nav-right d-flex align-items-center gap-5">
+            <div class="nav-action d-flex align-items-center gap-4">
+                <div class="cart">
+                    <a href="#">
+                        <i class="fas fa-shopping-cart"></i>
+                    </a>
+                </div>
+                <div class="notification">
+                    <a href="#">
+                        <i class="fas fa-bell"></i>
+                    </a>
+                </div>
+                <div class="mail">
+                    <a href="#">
+                        <i class="fas fa-envelope"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="divider"></div>
+            <div class="nav-auth">
+                <img src="https://avatars.githubusercontent.com/u/118147438?v=4" alt="">
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ components/splash-screen.php
@@ -0,0 +1,5 @@
+<div id="splash-screen">
+    <div class="splash-content">
+        <a class="navbar-brand fw-bold fs-3" href="#">Madura<span class="text-danger fw-bold fs-6">.shop</span></a>
+    </div>
+</div>
