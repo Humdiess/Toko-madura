f572e3e2410e520bd97abe10037c1961217f0b88
Aimannawal <aone111181@gmail.com>
2024-08-26 23:34:34 +0700

    feat: add sql part 2"

--- toko_madura.sql
+++ toko_madura.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 26, 2024 at 02:24 PM
+-- Generation Time: Aug 26, 2024 at 06:33 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -71,23 +71,31 @@ CREATE TABLE `orders` (
   `customer_name` varchar(100) NOT NULL,
   `customer_email` varchar(100) NOT NULL,
   `total_price` decimal(10,2) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `payment_method` enum('Transfer Bank','E-Wallet') NOT NULL DEFAULT 'Transfer Bank'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `user_id`, `customer_name`, `customer_email`, `total_price`, `created_at`) VALUES
-(1, 1, '123', '123@gmail', 100.00, '2024-08-24 15:48:31'),
-(2, 1, '123', '123@gmail', 0.00, '2024-08-26 12:12:25'),
-(3, 1, '123', '123@gmail', 0.00, '2024-08-26 12:14:06'),
-(4, 1, '123', '123@gmail', 0.00, '2024-08-26 12:14:27'),
-(5, 1, '123', '123@gmail', 0.00, '2024-08-26 12:15:08'),
-(6, 1, '123', '123@gmail', 0.00, '2024-08-26 12:19:59'),
-(7, 1, '123', '123@gmail', 0.00, '2024-08-26 12:21:20'),
-(8, 1, '123', '123@gmail', 99999999.99, '2024-08-26 12:22:05'),
-(9, 1, '123', '123@gmail', 10000000.00, '2024-08-26 12:22:58');
+INSERT INTO `orders` (`id`, `user_id`, `customer_name`, `customer_email`, `total_price`, `created_at`, `payment_method`) VALUES
+(35, 4, 'skomda-siswa', '123456@gmail', 10000000.00, '2024-08-26 16:08:50', ''),
+(36, 4, 'skomda-siswa', '123456@gmail', 60000000.00, '2024-08-26 16:09:57', ''),
+(37, 4, 'skomda-siswa', '123456@gmail', 50000000.00, '2024-08-26 16:10:06', ''),
+(38, 4, 'skomda-siswa', '123456@gmail', 10000000.00, '2024-08-26 16:12:25', ''),
+(39, 4, 'skomda-siswa', '123456@gmail', 10000000.00, '2024-08-26 16:12:51', ''),
+(40, 4, 'skomda-siswa', '123456@gmail', 0.00, '2024-08-26 16:13:27', 'E-Wallet'),
+(41, 4, 'skomda-siswa', '123456@gmail', 10003400.00, '2024-08-26 16:14:45', ''),
+(42, 4, 'skomda-siswa', '123456@gmail', 10000000.00, '2024-08-26 16:14:56', 'E-Wallet'),
+(43, 4, 'skomda-siswa', '123456@gmail', 3400.00, '2024-08-26 16:15:10', 'Transfer Bank'),
+(44, 4, 'skomda-siswa', '123456@gmail', 10003400.00, '2024-08-26 16:15:22', 'Transfer Bank'),
+(45, 4, 'skomda-siswa', '123456@gmail', 3400.00, '2024-08-26 16:15:50', 'E-Wallet'),
+(46, 4, 'skomda-siswa', '123456@gmail', 10000000.00, '2024-08-26 16:24:12', ''),
+(47, 4, 'skomda-siswa', '123456@gmail', 0.00, '2024-08-26 16:24:24', ''),
+(48, 4, 'skomda-siswa', '123456@gmail', 0.00, '2024-08-26 16:24:33', ''),
+(49, 4, 'skomda-siswa', '123456@gmail', 6800.00, '2024-08-26 16:26:03', 'Transfer Bank'),
+(50, 4, 'skomda-siswa', '123456@gmail', 3400.00, '2024-08-26 16:30:46', 'Transfer Bank');
 
 -- --------------------------------------------------------
 
@@ -108,8 +116,21 @@ CREATE TABLE `order_items` (
 --
 
 INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`, `price`) VALUES
-(2, 8, 5, 37, 10000000.00),
-(3, 9, 5, 1, 10000000.00);
+(30, 35, 5, 1, 10000000.00),
+(31, 36, 5, 6, 10000000.00),
+(32, 37, 5, 5, 10000000.00),
+(33, 38, 5, 1, 10000000.00),
+(34, 39, 5, 1, 10000000.00),
+(35, 41, 5, 1, 10000000.00),
+(36, 41, 9, 1, 3400.00),
+(37, 42, 5, 1, 10000000.00),
+(38, 43, 9, 1, 3400.00),
+(39, 44, 5, 1, 10000000.00),
+(40, 44, 9, 1, 3400.00),
+(41, 45, 9, 1, 3400.00),
+(42, 46, 5, 1, 10000000.00),
+(43, 49, 9, 2, 3400.00),
+(44, 50, 9, 1, 3400.00);
 
 -- --------------------------------------------------------
 
@@ -132,7 +153,8 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `images`, `created_at`, `category_id`) VALUES
-(5, 'Musang Surabaya', 'Musang adalah hewan hitam seperti di foto', 10000000.00, 'WIN_20240730_10_54_48_Pro.jpg,WIN_20240730_10_55_15_Pro.jpg,WIN_20240730_10_55_42_Pro.jpg', '2024-08-25 02:06:31', 6);
+(5, 'Musang Surabaya', 'Musang adalah hewan hitam seperti di foto', 10000000.00, 'WIN_20240730_10_54_48_Pro.jpg,WIN_20240730_10_55_15_Pro.jpg,WIN_20240730_10_55_42_Pro.jpg', '2024-08-25 02:06:31', 6),
+(9, 'akusukaeskrim', 'asdasdasdas', 3400.00, '1724470893889.jpg', '2024-08-26 16:03:31', 3);
 
 -- --------------------------------------------------------
 
@@ -216,7 +238,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `carts`
 --
 ALTER TABLE `carts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -228,19 +250,19 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
