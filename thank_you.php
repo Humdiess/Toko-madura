f737649a6f54277d1e80bf6f06926c37351536ec
Humdiess <hmzinchannel@gmail.com>
2024-08-27 09:18:37 +0700

    revert thankyou

--- thank_you.php
+++ thank_you.php
@@ -1,10 +1,15 @@
 <?php
 session_start();
 ?>
-<?php include('themes/home/header.php'); ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Thank You</title>
+</head>
+<body>
     <h1>Thank You for Your Order!</h1>
     <p>Your order has been placed successfully. We will contact you shortly.</p>
     <a href="index.php">Return to Home</a>
-
-<?php include('themes/home/header.php'); ?>
+</body>
+</html>
